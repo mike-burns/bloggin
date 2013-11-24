004a85ab84ab981f0cbf6b9234e92ccb02c09cb2
Mike Burns <mike@mike-burns.com>
2013-11-24 11:16:48 +0100

    Initial commit
    This is a quick sample Rails app that I can use for trying Elastx.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bloggin::Application.config.secret_token = '4780e11b777a2663a3905b2ddd77528d1b7997671218fbaba6d7f4db942f512986ee486cc80cb302903d544afae22efd514a73f47bfe532a16d7e4a8ccd3fe99'
